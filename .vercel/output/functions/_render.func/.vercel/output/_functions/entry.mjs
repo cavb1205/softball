2eecfba8e1001381097eff93ad9f3880b0b34def
cavb1205 <cavb.1205@hotmail.com>
2024-11-26 15:44:37 -0300

    init

---
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/entry.mjs
@@ -0,0 +1,26 @@
+import { renderers } from './renderers.mjs';
+import { c as createExports } from './chunks/entrypoint_CAUtyYMm.mjs';
+import { manifest } from './manifest_Bxm-NUWz.mjs';
+
+const _page0 = () => import('./pages/_image.astro.mjs');
+const _page1 = () => import('./pages/index.astro.mjs');
+
+const pageMap = new Map([
+    ["node_modules/astro/dist/assets/endpoint/generic.js", _page0],
+    ["src/pages/index.astro", _page1]
+]);
+const serverIslandMap = new Map();
+const _manifest = Object.assign(manifest, {
+    pageMap,
+    serverIslandMap,
+    renderers,
+    middleware: () => import('./_noop-middleware.mjs')
+});
+const _args = {
+    "middlewareSecret": "aff5b95c-c486-43c7-be36-fad983f940d8",
+    "skewProtection": false
+};
+const _exports = createExports(_manifest, _args);
+const __astrojsSsrVirtualEntry = _exports.default;
+
+export { __astrojsSsrVirtualEntry as default, pageMap };

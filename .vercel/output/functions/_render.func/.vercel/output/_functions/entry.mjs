b5bea27f1a15da8a79eaa23b7e4faaa0f54c1b81
cavb1205 <cavb.1205@hotmail.com>
2024-11-26 20:03:16 -0300

    deploy update restart

--- .vercel/output/functions/_render.func/.vercel/output/_functions/entry.mjs
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports } from './chunks/entrypoint_CAUtyYMm.mjs';
-import { manifest } from './manifest_Bxm-NUWz.mjs';
+import { manifest } from './manifest_qp3_aiUw.mjs';
 
 const _page0 = () => import('./pages/_image.astro.mjs');
 const _page1 = () => import('./pages/index.astro.mjs');
@@ -17,7 +17,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "aff5b95c-c486-43c7-be36-fad983f940d8",
+    "middlewareSecret": "7926cb87-a09a-40aa-ac08-14db6ca0f168",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
